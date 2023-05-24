eded373e00e7c7919bf3c44cd51900225fd88b99
Bartek Kowalski <poprostubartek.official@gmail.com>
2023-05-25 00:48:45 +0200

    Implement PhotoMetadata class

--- app/src/main/java/com/example/photoeditor/photo_classes/PhotoMetadata.kt
+++ app/src/main/java/com/example/photoeditor/photo_classes/PhotoMetadata.kt
@@ -1,4 +1,23 @@
 package com.example.photoeditor.photo_classes
 
-class PhotoMetadata(val resolution: Pair<Int, Int>, val name: String, val timestamp: Int) {
+import java.text.SimpleDateFormat
+import java.util.Date
+import kotlin.reflect.typeOf
+
+class PhotoMetadata(private val name: String,
+                    private val resolution: Pair<Int, Int>,
+                    private val timestamp: Long) {
+
+
+    val bundled: HashMap<String, Any>
+        get() {
+            val data: HashMap<String, Any> = HashMap<String, Any>()
+
+            data["Name"] = this.name
+            data["Resolution"] = this.resolution.first.toString() + " x " + this.resolution.second.toString()
+            data["Date"] =  SimpleDateFormat("dd-MM-yyyy").format(Date(this.timestamp))
+
+            return data
+        }
+
 }
