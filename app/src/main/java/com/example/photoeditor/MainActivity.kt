08478fc6357f47d723dbf6160cf8c7ba37f0f9e4
Bartek Kowalski <poprostubartek.official@gmail.com>
2023-08-23 05:59:55 +0200

    Add Change Image and Save Image buttons

--- app/src/main/java/com/example/photoeditor/MainActivity.kt
+++ app/src/main/java/com/example/photoeditor/MainActivity.kt
@@ -1,9 +1,21 @@
 package com.example.photoeditor
 
+import android.content.ContentValues
+import android.content.DialogInterface
 import android.graphics.Bitmap
+import android.graphics.BitmapFactory
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Environment
+import android.os.ParcelFileDescriptor
+import android.provider.MediaStore
 import android.util.Log
+import android.view.View
+import android.webkit.URLUtil
+import androidx.appcompat.app.AlertDialog
+import androidx.core.content.ContentProviderCompat.requireContext
+import androidx.fragment.app.FragmentManager
 import com.example.photoeditor.databinding.ActivityMainBinding
 import com.example.photoeditor.photo_classes.EffectType
 import com.example.photoeditor.photo_classes.IEffect
@@ -12,6 +24,10 @@ import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.coroutineScope
 import kotlinx.coroutines.launch
+import java.io.FileDescriptor
+import java.time.LocalDate
+import java.time.LocalDateTime
+import java.util.*
 import kotlin.system.measureTimeMillis
 import kotlin.time.measureTime
 
@@ -25,12 +41,57 @@ class MainActivity : AppCompatActivity(), FileSelectedListener, EffectSelectedLi
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        binding.buttonChangeImage.setOnClickListener {
+            val dialog = createChangeImageDialog()
+            dialog.show()
+        }
+        binding.buttonSaveImage.setOnClickListener {
+            val dialog = createSaveImageDialog()
+            dialog.show()
+        }
         //setting the menu fragment container to photo importing fragment
         supportFragmentManager.beginTransaction().apply {
             replace(binding.fcvMenu.id, PhotoImportMenu())
-            addToBackStack(null)
             commit()
         }
+    }
+
+    private fun createChangeImageDialog(): AlertDialog {
+        val builder = AlertDialog.Builder(this)
+        builder.setMessage("Utracisz wszystkie niezapisane zmiany!\nCzy chcesz kontynuować?")
+            .setPositiveButton("TAK", DialogInterface.OnClickListener { dialogInterface, i ->
+                photo = null
+                binding.ivPhoto.setImageResource(R.drawable.empty_view)
+                binding.topButtons.visibility = View.GONE
+                supportFragmentManager.popBackStack(null, FragmentManager.POP_BACK_STACK_INCLUSIVE)
+                supportFragmentManager.beginTransaction().apply {
+                    replace(binding.fcvMenu.id, PhotoImportMenu())
+                    commit()
+                }
+            })
+            .setNegativeButton("NIE", null)
+        return builder.create()
+
+    }
+
+    private fun createSaveImageDialog(): AlertDialog {
+        val builder = AlertDialog.Builder(this)
+        builder.setMessage("Utracisz wszystkie niezapisane zmiany!\nCzy chcesz kontynuować?")
+            .setPositiveButton("ZAPISZ", DialogInterface.OnClickListener { dialogInterface, i ->
+                val name = "${UUID.randomUUID()}.png"
+                val contentValues = ContentValues().apply {
+                    put(MediaStore.MediaColumns.DISPLAY_NAME, name)
+                    put(MediaStore.MediaColumns.MIME_TYPE, "image/png")
+                    put(MediaStore.MediaColumns.RELATIVE_PATH, "${Environment.DIRECTORY_DCIM}/PhotoEditor")
+                }
+                contentResolver.let { it ->
+                    it.insert(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, contentValues)?.let{ uri ->
+                        it.openOutputStream(uri)?.let{photo!!.preview!!.compress(Bitmap.CompressFormat.PNG,100, it)}
+                    }
+                }
+            })
+            .setNegativeButton("ANULUJ", null)
+        return builder.create()
     }
 
     override fun onFileSelected(image: Bitmap) {
@@ -38,7 +99,7 @@ class MainActivity : AppCompatActivity(), FileSelectedListener, EffectSelectedLi
         photo = Photo(image)
 
         photo!!.original?.let { updateImageView(it) }
-
+        binding.topButtons.visibility = View.VISIBLE
         supportFragmentManager.beginTransaction().apply{
             replace(binding.fcvMenu.id, EffectsMenu())
             addToBackStack(null)
