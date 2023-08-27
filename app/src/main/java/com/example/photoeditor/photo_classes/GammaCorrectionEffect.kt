0011afeda7f344f48590f67f4eee8f5c797e7400
Bartek Kowalski <poprostubartek.official@gmail.com>
2023-08-27 18:03:39 +0200

    Optimize Gamma correction effect

--- app/src/main/java/com/example/photoeditor/photo_classes/GammaCorrectionEffect.kt
+++ app/src/main/java/com/example/photoeditor/photo_classes/GammaCorrectionEffect.kt
@@ -2,6 +2,9 @@ package com.example.photoeditor.photo_classes
 
 import android.graphics.Bitmap
 import android.graphics.Color
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.runBlocking
 import kotlin.math.pow
 
 class GammaCorrectionEffect(override val type: EffectType,
@@ -9,22 +12,35 @@ class GammaCorrectionEffect(override val type: EffectType,
                             private val value: Double) : IEffect {
 
     override fun modifyPhoto(bitmap: Bitmap): Bitmap {
-        val bm = bitmap.copy(bitmap.config, true)
+        val width = bitmap.width
+        val height = bitmap.height
+        //allocating two integer arrays, one for input image pixels and one for output image pixels
+        val pixels = IntArray(width * height)
+        val newPixels = IntArray(width * height)
+        //filling input image array with pixel values from the bitmap
+        bitmap.getPixels(pixels, 0, width, 0, 0, width, height)
         val coef = getCoefficient()
-        for(y in 0 until bm.height){
-            for(x in 0 until bm.width){
-                val colour = bm.getPixel(x,y)
+        runBlocking(Dispatchers.Default) {
+        for (y in 0 until height) {
+            launch {
+                for (x in 0 until width) {
+                    val colour = pixels[y * width + x]
 
-                var red = (255f * (Color.red(colour).toFloat()/255f).pow(coef)).toInt()
-                red = truncate(red)
-                var green = (255f * (Color.green(colour).toFloat()/255f).pow(coef)).toInt()
-                green = truncate(green)
-                var blue = (255f * (Color.blue(colour).toFloat()/255f).pow(coef)).toInt()
-                blue = truncate(blue)
-                bm.setPixel(x,y, Color.rgb(red,green,blue))
+                    var red = (255f * (Color.red(colour).toFloat() / 255f).pow(coef)).toInt()
+                    red = truncate(red)
+                    var green = (255f * (Color.green(colour).toFloat() / 255f).pow(coef)).toInt()
+                    green = truncate(green)
+                    var blue = (255f * (Color.blue(colour).toFloat() / 255f).pow(coef)).toInt()
+                    blue = truncate(blue)
+                    newPixels[y * width + x] = Color.rgb(red, green, blue)
+                }
             }
         }
-        return bm
+        }
+        // creating an output bitmap with calculated pixels
+        val newBitmap = Bitmap.createBitmap(width, height, bitmap.config)
+        newBitmap.setPixels(newPixels, 0, width, 0, 0, width, height)
+        return newBitmap
     }
 
     private fun getCoefficient(): Float{
